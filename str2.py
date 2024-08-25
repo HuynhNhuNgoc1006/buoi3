91ba1f0091aed6d0457d0abd970870c237de8673
HuynhNhuNgoc1006 <ngocfhfh12@gmail.com>
2024-08-25 16:27:44 +0700

    Add files via upload

---
+++ str2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 16:20:59 2024
+
+@author: Student
+"""
+
+import math
+str2="Đại học Quốc gia, Khu phố 6, P.Linh Trung, Q.Thủ Đức, TP.HCM"
+print(str2[0:16])
+print(str2[18:27])
+print(str2[31:41])
+print(str2[45:52])
+print(str2[54:62])
